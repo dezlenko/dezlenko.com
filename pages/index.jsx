0fe26e251b48d8c79510840c2abe8dac30a6d781
dezlenko <dezlenko.main@gmail.com>
2022-04-18 23:13:49 -0400

    Add nav and hero

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -6,6 +7,27 @@ export default function Home() {
       <Head>
         <title>Home</title>
       </Head>
+      <main className="container home">
+        <nav className="flex row center v">
+          <Link href="/">Home</Link>
+        </nav>
+        <div className="hero">
+          <h1 className="large">
+            dezlenko<span className="color-pink">,</span>{' '}
+            where <span className="color-cyan">half</span>-
+            <span className="color-pink">assed</span> web
+            projects are born
+          </h1>
+          <p className="large">
+            And a bunch of other random things:
+            <br />
+            <strong>
+              widgets, black magic, tests, plugins... you
+              name it.
+            </strong>
+          </p>
+        </div>
+      </main>
     </>
   )
 }
